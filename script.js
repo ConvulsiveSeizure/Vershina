5d29e1170c30f44fe3f90d0bfd183c0d493242bf
Seizure <78146652+ConvulsiveSeizure@users.noreply.github.com>
2021-02-10 19:35:55 +0300

    some animations, cards, card tickets

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
 
 $(document).ready(() => {
-    //$("#SunLayer img").animate({"opacity": 1}, 1500)
+    $(".bilet").on("click", (e) => {
+        e.preventDefault()
+        $(e.target).removeClass("noactive")
+        $(".noactive").hide(1000)
+        let currentCardId = e.target.getAttribute("data-id")
+        $(`.cardTicket[data-id=${currentCardId }]`).css("opacity", 1)
+        $(".cardTicketsAbs").addClass("frontView")
+       
+    })
+    $(".closeTicket").on("click", (e) => {
+        e.preventDefault()
+        $(".cardTicket").css("opacity", 0)
+        $(".bilet").addClass("noactive")
+        $(".noactive").show(500)
+        $(".cardTicketsAbs").removeClass("frontView")
+    })
 })
